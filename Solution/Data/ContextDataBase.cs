cd5925aa591dd8a4b2a178e691c550358909fb43
MasyJordan <masyjordan@gmail.com>
2025-07-15 16:30:59 +0200

    tailwind fonctionnel

--- Solution/Data/ContextDataBase.cs
+++ Solution/Data/ContextDataBase.cs
@@ -1,19 +1,22 @@
-namespace Solution.Data;
-
-//DbContext formule de base integree a entity
-public class ContextDataBase(DbContextOptions<ContextDataBase> options) : DbContext(options)
-{
-    public virtual DbSet<User> Users { get; set; }
-    public virtual DbSet<Role> Roles { get; set; }
-    public virtual DbSet<Offer> Offers { get; set; }
-
-
-    //Cette méthode configure la structure de votre base de données via l'API Fluent.
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<User>(entity =>
-        {
-            entity.Haskey(u => u.Id);
-        });
-    }
-}
+// using Microsoft.EntityFrameworkCore;
+// using Solution.Models;
+//
+// namespace Solution.Data;
+//
+// //DbContext formule de base integree a entity
+// public class ContextDataBase(DbContextOptions<ContextDataBase> options) : DbContext(options)
+// {
+//     public virtual DbSet<User> Users { get; set; }
+//     public virtual DbSet<Role> Roles { get; set; }
+//     public virtual DbSet<Offer> Offers { get; set; }
+//
+//
+//     //Cette méthode configure la structure de votre base de données via l'API Fluent.
+//     protected override void OnModelCreating(ModelBuilder modelBuilder)
+//     {
+//         modelBuilder.Entity<User>(entity =>
+//         {
+//             entity.Haskey(u => u.Id);
+//         });
+//     }
+// }
